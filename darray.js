c310547ff4b9375ec7cdd8ce6a1fa33c5c2084b0
caleb <caleb.geek.gk@gmail.com>
2020-08-20 20:14:21 +0300

    python solution

--- darray.js
+++ darray.js
@@ -55,5 +55,18 @@ let total= f_result.map(function (num, idx) {
 
   let max = Math.max(...f_total);
 console.log(max)
-// console.log(f_total)
 
+
+// python solution
+
+/*
+sum = []
+
+
+for i in range(len(arr)-2):
+    for j in range(len(arr)-2):
+        sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+        
+print(max(sum))
+
+*/
